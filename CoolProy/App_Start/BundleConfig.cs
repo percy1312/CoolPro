da6f8978b7cd196ebf2580bb1c9a3ead6bb76aef
percy1312 <percy1312@hotmail.com>
2016-04-08 00:34:30 -0500

    2
    2

--- CoolProy/App_Start/BundleConfig.cs
+++ CoolProy/App_Start/BundleConfig.cs
@@ -14,9 +14,9 @@ namespace CoolProy
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.unobtrusive*",
+            //            "~/Scripts/jquery.validate*"));
 
             // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información sobre los formularios. De este modo, estará
             // preparado para la producción y podrá utilizar la herramienta de creación disponible en http://modernizr.com para seleccionar solo las pruebas que necesite.
